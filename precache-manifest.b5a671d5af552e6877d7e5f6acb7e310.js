a1a345212fb0d359a35127dd1828963efd5a2ce3
Sinha1910 <sugandhasinha.5252@gmail.com>
2020-01-28 20:57:26 +0530

    Updates

---
+++ precache-manifest.b5a671d5af552e6877d7e5f6acb7e310.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ab04952c357a79c0121e7cc874d2f43",
+    "url": "/ShoppingCart/index.html"
+  },
+  {
+    "revision": "fb44331005f347b804ae",
+    "url": "/ShoppingCart/static/css/main.84667386.chunk.css"
+  },
+  {
+    "revision": "8a38402ec260463c1b26",
+    "url": "/ShoppingCart/static/js/2.d020dd76.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/ShoppingCart/static/js/2.d020dd76.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fb44331005f347b804ae",
+    "url": "/ShoppingCart/static/js/main.07d66697.chunk.js"
+  },
+  {
+    "revision": "7271c6813b0920c07e88",
+    "url": "/ShoppingCart/static/js/runtime-main.21cc0467.js"
+  }
+]);
